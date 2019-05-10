8802163a2990fcd620f2e65cf70ab6a1bd6896e3
r3d9u11 <r3d9u11@gmail.com>
2019-05-10 10:42:58 +0300

    readme_info

--- export.sh
+++ export.sh
@@ -6,14 +6,14 @@ source ./setEnv.sh
 
 function export_app() {
     APPDIR=$Qt_DIR/$(basename $Qt_EXPORT)
-
+    
     if rsync -a -r "$Qt_TEST/$1/$1" "$Qt_EXPORT/" ; then
         if rsync -a -r "$Qt_TEST/launcher.sh" "$Qt_EXPORT/$1.sh" ; then
             sed -i "s/Qt_DIR=./Qt_DIR=${Qt_DIR//\//\\/}/g" "$Qt_EXPORT/$1.sh"
             sed -i "s/APPDIR=./APPDIR=${APPDIR//\//\\/}/g" "$Qt_EXPORT/$1.sh"
-    
+            
             echo "    EXPORTED: $(basename $Qt_EXPORT/$1)"
-    
+            
             return 0
         fi
     fi
